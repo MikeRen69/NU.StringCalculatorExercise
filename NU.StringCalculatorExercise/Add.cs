b42798d79d5817dd5b1b55f738f2f4c4eb9e818d
michaelrennie <32267611+michaelrennie@users.noreply.github.com>
2019-02-20 07:56:11 +0000

    Complete step 5

--- NU.StringCalculatorExercise/Add.cs
+++ NU.StringCalculatorExercise/Add.cs
@@ -6,9 +6,36 @@ namespace NU.StringCalculatorExercise
 {
     public class StringCalculator
     {
+        // Steps 1 & 2
+        //public int Add(string numbers)
+        //{
+        //    List<string> result = numbers.Split(",").ToList();
+
+        //    return result.Sum(e => String.IsNullOrEmpty(e) ? 0 : Convert.ToInt32(e));
+        //}
+
+        // Step 3
+        //public int Add(string numbers)
+        //{
+        //    if(numbers.Contains(",\n")) { return 0; }
+
+        //    List<string> result = numbers.Replace("\n",",").Split(",").ToList();
+
+        //    return result.Sum(e => String.IsNullOrEmpty(e) ? 0 : Convert.ToInt32(e));
+        //}
+
+        // Step 4
         public int Add(string numbers)
         {
-            List<string> result = numbers.Split(",").ToList();
+            string delimiter = ",";
+            if (numbers.StartsWith(@"//"))
+            {
+                delimiter = numbers.Substring(2, numbers.IndexOf(@"\n") - 2);
+                numbers = numbers.Substring(numbers.IndexOf(@"\n")+2);
+            }
+            if (numbers.Contains(delimiter + "\n")) { throw new ArgumentException("New Line found in wrong location"); }
+
+            List<string> result = numbers.Replace("\n", delimiter).Split(delimiter).ToList();
 
             return result.Sum(e => String.IsNullOrEmpty(e) ? 0 : Convert.ToInt32(e));
         }
--- NUnitTest.NU.StringCalculatorExercise/UnitTest1.cs
+++ NUnitTest.NU.StringCalculatorExercise/UnitTest1.cs
@@ -19,6 +19,9 @@ namespace Tests
         [Test]
         public void TestExercise1()
         {
+
+            // Step 1 Tests
+
             int resultTest1 = _objStringCalculator.Add("");
 
             Assert.True(resultTest1 == 0);
@@ -31,6 +34,24 @@ namespace Tests
 
             Assert.True(resultTest3 == 3);
 
+            // Step 2 Tests
+            int resultTest4 = _objStringCalculator.Add("1,2,3,4,5,6,7,8");
+
+            Assert.True(resultTest4 == 36);
+
+            // Step 3 Tests
+            int resultTest5 = _objStringCalculator.Add("1\n2,3");
+
+            Assert.True(resultTest5 == 6);
+
+            //int resultTest6 = _objStringCalculator.Add("1,\n");
+
+            //Assert.True(resultTest6 == 0);
+
+            // Step 4 Tests
+            int resultTest7 = _objStringCalculator.Add(@"//;\n1;2");
+
+            Assert.True(resultTest7 == 3);
         }
     }
 }
