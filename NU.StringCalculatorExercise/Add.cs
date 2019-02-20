4f08b20aecb65062d3b90847fb1dd658502dd854
MikeRen69 <rennie1969@gmail.com>
2019-02-20 10:49:31 +0000

    Complete all steps

--- NU.StringCalculatorExercise/Add.cs
+++ NU.StringCalculatorExercise/Add.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 
 namespace NU.StringCalculatorExercise
 {
@@ -25,20 +26,108 @@ namespace NU.StringCalculatorExercise
         //}
 
         // Step 4
+        //public int Add(string numbers)
+        //{
+        //    string delimiter = ",";
+        //    if (numbers.StartsWith(@"//"))
+        //    {
+        //        delimiter = numbers.Substring(2, numbers.IndexOf(@"\n") - 2);
+        //        numbers = numbers.Substring(numbers.IndexOf(@"\n")+2);
+        //    }
+        //    if (numbers.Contains(delimiter + "\n")) { throw new ArgumentException("New Line found in wrong location"); }
+
+        //    List<string> result = numbers.Replace("\n", delimiter).Split(delimiter).ToList();
+
+        //    return result.Sum(e => String.IsNullOrEmpty(e) ? 0 : Convert.ToInt32(e));
+        //}
+
+        // Step 5
+        //public int Add(string numbers)
+        //{
+        //    string delimiter = ",";
+        //    if (numbers.StartsWith(@"//"))
+        //    {
+        //        delimiter = numbers.Substring(2, numbers.IndexOf(@"\n") - 2);
+        //        numbers = numbers.Substring(numbers.IndexOf(@"\n") + 2);
+        //    }
+        //    if (numbers.Contains(delimiter + "\n")) { throw new ArgumentException("New Line found in wrong location"); }
+
+        //    List<string> result = numbers.Replace("\n", delimiter).Split(delimiter).ToList();
+
+        //    // Check for negatives
+        //    StringBuilder negatives = new StringBuilder();
+        //    foreach (string item in result.Where(n=> !String.IsNullOrEmpty(n) && Convert.ToInt32(n) < 0))
+        //        negatives.AppendLine(item);
+
+        //    if(negatives.Length > 0)
+        //        throw new ArgumentException("negatives not allowed\r\n" + negatives.ToString());
+
+
+        //    return result.Sum(e => String.IsNullOrEmpty(e) ? 0 : Convert.ToInt32(e));
+        //}
+
+        // Step 6
+        //public int Add(string numbers)
+        //{
+        //    string delimiter = ",";
+        //    if (numbers.StartsWith(@"//"))
+        //    {
+        //        delimiter = numbers.Substring(2, numbers.IndexOf(@"\n") - 2);
+        //        numbers = numbers.Substring(numbers.IndexOf(@"\n") + 2);
+        //    }
+        //    if (numbers.Contains(delimiter + "\n")) { throw new ArgumentException("New Line found in wrong location"); }
+
+        //    List<string> result = numbers.Replace("\n", delimiter).Split(delimiter).ToList();
+
+        //    // Check for negatives
+        //    StringBuilder negatives = new StringBuilder();
+        //    foreach (string item in result.Where(n => !String.IsNullOrEmpty(n) && Convert.ToInt32(n) < 0))
+        //        negatives.AppendLine(item);
+
+        //    if (negatives.Length > 0)
+        //        throw new ArgumentException("negatives not allowed\r\n" + negatives.ToString());
+
+        //    return result.Where(t => !String.IsNullOrEmpty(t) && Convert.ToInt32(t) < 1001).Sum(e => Convert.ToInt32(e));
+        //}
+
+        // Step 7-9
         public int Add(string numbers)
         {
             string delimiter = ",";
-            if (numbers.StartsWith(@"//"))
+            if (numbers.StartsWith(@"//["))
             {
-                delimiter = numbers.Substring(2, numbers.IndexOf(@"\n") - 2);
-                numbers = numbers.Substring(numbers.IndexOf(@"\n")+2);
+                var cleanDelimiter = numbers.Substring(3, numbers.IndexOf(@"\n") - 4).Replace("]", "");
+                numbers = numbers.Substring(numbers.IndexOf(@"\n") + 2);
+                List<string> lstDelimiter = cleanDelimiter.Split("[").ToList();
+
+                delimiter = lstDelimiter.FirstOrDefault();
+
+                // For simplicity replace all delimiters with the first delimiter
+                foreach (string item in lstDelimiter)
+                    numbers = numbers.Replace(item, delimiter);
             }
+            else
+            {
+                if (numbers.StartsWith(@"//"))
+                {
+                    delimiter = numbers.Substring(2, numbers.IndexOf(@"\n") - 2);
+                    numbers = numbers.Substring(numbers.IndexOf(@"\n") + 2);
+                }
+            }
+
             if (numbers.Contains(delimiter + "\n")) { throw new ArgumentException("New Line found in wrong location"); }
 
             List<string> result = numbers.Replace("\n", delimiter).Split(delimiter).ToList();
 
-            return result.Sum(e => String.IsNullOrEmpty(e) ? 0 : Convert.ToInt32(e));
-        }
+            // Check for negatives
+            StringBuilder negatives = new StringBuilder();
+            foreach (string item in result.Where(n => !String.IsNullOrEmpty(n) && Convert.ToInt32(n) < 0))
+                negatives.AppendLine(item);
 
+            if (negatives.Length > 0)
+                throw new ArgumentException("negatives not allowed\r\n" + negatives.ToString());
+
+            return result.Where(t => !String.IsNullOrEmpty(t) && Convert.ToInt32(t) < 1001).Sum(e => Convert.ToInt32(e));
+        }
     }
 }
--- NUnitTest.NU.StringCalculatorExercise/UnitTest1.cs
+++ NUnitTest.NU.StringCalculatorExercise/UnitTest1.cs
@@ -13,7 +13,7 @@ namespace Tests
 
         [SetUp]
         public void Setup()
-        { 
+        {
         }
 
         [Test]
@@ -52,6 +52,31 @@ namespace Tests
             int resultTest7 = _objStringCalculator.Add(@"//;\n1;2");
 
             Assert.True(resultTest7 == 3);
+
+            // Step 5 Tests
+            //int resultTest8 = _objStringCalculator.Add(@"-100");
+
+            //Assert.True(resultTest8 == 0);
+
+            // Step 6 Tests
+            int resultTest9 = _objStringCalculator.Add(@"1001,1000,10");
+
+            Assert.True(resultTest9 == 1010);
+
+            // Step 7 Tests
+            int resultTest10 = _objStringCalculator.Add(@"//[***]\n1***2***3");
+
+            Assert.True(resultTest10 == 6);
+
+            // Step 8 Tests
+            int resultTest11 = _objStringCalculator.Add(@"//[*][%]\n1*2%3");
+
+            Assert.True(resultTest11 == 6);
+
+            // Step 9 Tests
+            int resultTest12 = _objStringCalculator.Add(@"//[***][%%%]\n1***2%%%3");
+
+            Assert.True(resultTest12 == 6);
         }
     }
 }
